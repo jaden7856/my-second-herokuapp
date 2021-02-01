66f33fa557d00660feb36bdd8ae0d223b500bd9c
Jaden7856 <jhg7856@gmail.com>
2021-02-01 16:40:54 +0900

    second heroku app connect test

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+// http://127.0.0.1:3000/
+app.get('/', (req, res) => res.send('Hello, Nodejs!'));
+app.get('/welcome', (req, res) => {
+    var user_name = req.param('name');
+    res.send('Hi, there. Welcome to the Nodejs service. [' + user_name + ']');
+});
+
+app.listen(port, () => console.log('Example app listening on port 3000'))
